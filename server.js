cfc7f77d533ba441ad5307ab92a8d53e8e7ffe7a
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-20 15:31:22 -0800

    completed minimum requirements

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 var Express = require('express');
+var fs = require('fs');
 var userFile = require('./users');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var app = Express();
 
-// app.use(Express.static(__dirname+"/www"));
-// app.use(Express.static(__dirname+"/js"));
+app.use(Express.static(__dirname+"/www"));
+app.use(Express.static(__dirname+"/js"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
-
-
-app.get("/", function(req, res){
-    res.send("hello world");
-});
 
 app.get("/users",function(req,res) {
   res.send(userFile);
 })
+
+app.get("/users/:id",function(req,res, next) {
+  var userId = req.params.id;
+  res.send(userFile[userId])
+})
+
+app.post("/users", function(req, res, next) {
+  // userFile.push(userId);
+  console.log(req.body);
+  userFile.push(req.body)
+  fs.writeFileSync("./users.json",JSON.stringify(userFile));
+  // fs.appendFileSync("./users.json",user);
+});
+
+
 
 var port = "8080";
 var server = app.listen(port);
